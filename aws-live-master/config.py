0777b7f0d8336b049c73eb3720e2038c09bf3ff7
tkavi <79509881+tkavi@users.noreply.github.com>
2021-03-25 15:25:04 +0530

    commit

---
+++ aws-live-master/config.py
@@ -0,0 +1,6 @@
+customhost = "demodb.cxlv841n6d6t.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "password"
+customdb = "employee"
+custombucket = "media-bucket-2021"
+customregion = "ap-south-1"
